7bd7a97f8ac361714a9c765803b1007ad3095491
Chow1996 <53964335+Chow1996@users.noreply.github.com>
2025-12-13 20:02:36 +0800

    Auto update: 2025-12-13 12:02:36

--- data.js
+++ data.js
@@ -733,7 +733,7 @@ const cexListings = [
         exchange: 'Bithumb',
         type: 'spot',
         time: '13:46',
-        notes: '📢 **Bithumb LISTING:[이벤트] 후마 파이낸스(HUMA), 포르타(FORT) 원화 마켓 추가 기념 에어드랍 이벤트 **  Bithumb LISTING:[活动] HUMA Finance（后马金融）庆祝FORT（音译为\\\\\\\\\\\\\\\\\\\\\\"福尔',
+        notes: '📢 **Bithumb LISTING:[이벤트] 후마 파이낸스(HUMA), 포르타(FORT) 원화 마켓 추가 기념 에어드랍 이벤트 **  Bithumb LISTING:[活动] HUMA Finance（后马金融）庆祝FORT（音译为\\\\\\\\\\\\\\\\\\\\\\\"福',
     },
     {
         date: '2025-06-19',
@@ -742,7 +742,7 @@ const cexListings = [
         exchange: 'Bithumb',
         type: 'spot',
         time: '13:46',
-        notes: '📢 **Bithumb LISTING:[이벤트] 후마 파이낸스(HUMA), 포르타(FORT) 원화 마켓 추가 기념 에어드랍 이벤트 **  Bithumb LISTING:[活动] HUMA Finance（后马金融）庆祝FORT（音译为\\\\\\\\\\\\\\\\\\\\\\"福尔',
+        notes: '📢 **Bithumb LISTING:[이벤트] 후마 파이낸스(HUMA), 포르타(FORT) 원화 마켓 추가 기념 에어드랍 이벤트 **  Bithumb LISTING:[活动] HUMA Finance（后马金融）庆祝FORT（音译为\\\\\\\\\\\\\\\\\\\\\\\"福',
     },
     {
         date: '2025-06-19',
@@ -5716,7 +5716,7 @@ const cexListings = [
         exchange: 'Binance',
         type: 'perp',
         time: '16:43',
-        notes: '📢 **Binance Futures Will Launch USDⓈ-Margined RLSUSDT Perpetual Contract (2025-12-02) **  \\\\\\\\\\\\\\\\\\\\\"币安期货将推出以美元Ⓢ计价的RLSUSDT永续合约（2025年12月2日）\',
+        notes: '📢 **Binance Futures Will Launch USDⓈ-Margined RLSUSDT Perpetual Contract (2025-12-02) **  \\\\\\\\\\\\\\\\\\\\\\"币安期货将推出以美元Ⓢ计价的RLSUSDT永续合约（2025年12月2日）',
     },
     {
         date: '2025-12-03',
@@ -6001,5 +6001,14 @@ const cexListings = [
         type: 'perp',
         time: '17:17',
         notes: '📢 **Binance Futures Will Launch USDⓈ-Margined USUSDT and CYSUSDT Perpetual Contracts (2025-12-12) **  币安期货将于2025年12月12日推出以美元Ⓢ为保证金的USUSDT和CYSUSDT永续合约  ',
+    },
+    {
+        date: '2025-12-13',
+        token: 'LIGHTER',
+        token_display: 'Lighter (LIGHTER)',
+        exchange: 'Coinbase',
+        type: 'spot',
+        time: '07:28',
+        notes: '📢 **COINBASE LISTING: Assets added to the roadmap today: Lighter (LIGHTER) **  COINBASE LISTING: 今天添加到产品路线图的资产：Lighter（轻量级）  ---------- 🔗 [查看来源](https',
     },
 ];
